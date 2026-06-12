74229eaa780fc34de6509d31b69f3b0ef62c78ad
Luis Lenes <luislenes02@gmail.com>
2026-06-11 19:22:14 -0500

    Migrate CharacterViewModel to use cachedIn PagingData flow

--- app/src/main/java/com/luislenes/rickandmorty/presentation/list/CharactersUiState.kt
+++ app/src/main/java/com/luislenes/rickandmorty/presentation/list/CharactersUiState.kt
@@ -1,9 +1,6 @@
 package com.luislenes.rickandmorty.presentation.list
 
-import com.luislenes.rickandmorty.model.Character
-
 sealed class CharactersUiState {
     data object Loading : CharactersUiState()
-    data class Success(val characters: List<Character>) : CharactersUiState()
     data class Error(val message: String) : CharactersUiState()
 }
