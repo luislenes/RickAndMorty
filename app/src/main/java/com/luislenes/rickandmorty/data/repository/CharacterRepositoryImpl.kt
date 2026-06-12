6c7ec9af86431ec45ee85b31cdae81d16950216e
Luis Lenes <luislenes02@gmail.com>
2026-06-11 19:21:51 -0500

    Implement getCharactersStream using Pager in CharacterRepositoryImpl

--- app/src/main/java/com/luislenes/rickandmorty/data/repository/CharacterRepositoryImpl.kt
+++ app/src/main/java/com/luislenes/rickandmorty/data/repository/CharacterRepositoryImpl.kt
@@ -1,20 +1,34 @@
 package com.luislenes.rickandmorty.data.repository
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import com.luislenes.rickandmorty.data.remote.api.RickAndMortyApi
 import com.luislenes.rickandmorty.data.remote.dto.CharacterDto
+import com.luislenes.rickandmorty.data.remote.paging.CharacterPagingSource
 import com.luislenes.rickandmorty.model.Character
 import com.luislenes.rickandmorty.model.repository.CharacterRepository
+import kotlinx.coroutines.flow.Flow
 
 class CharacterRepositoryImpl(
     private val api: RickAndMortyApi
 ) : CharacterRepository {
 
-    override suspend fun getCharacters(): Result<List<Character>> = runCatching {
-        api.getCharacters().results.map { it.toDomain() }
-    }
+    override fun getCharactersStream(): Flow<PagingData<Character>> =
+        Pager(
+            config = PagingConfig(
+                pageSize           = PAGE_SIZE,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = { CharacterPagingSource(api) }
+        ).flow
 
     override suspend fun getCharacterById(id: Int): Result<Character> = runCatching {
         api.getCharacterById(id).toDomain()
+    }
+
+    companion object {
+        private const val PAGE_SIZE = 20
     }
 }
 
